b9ffa3ef43400c9d4984b75031383e4f4296a461
cherylregar <119674729+cherylregar@users.noreply.github.com>
2024-06-27 20:12:39 +0700

    Perbaikan CSS

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -3,60 +3,63 @@
 @section('title', 'Landing Page')
 
 @section('content')
-    <div class="containerawal">
-        <p>Kami disini untuk <span class="highlight">melayani</span>,<br> dan <span class="highlight">menyelamatkan</span> bumi!</p>
-        <div class="buttonmemilah">
-            <button class="start-button">Mulai Memilah</button>
-        </div>
-    </div>
 
-    <div class="boxjualan">
-        
-        <div class="bjcon1">
-            <div class="jenisdijual">
-                <div class="concontainer">
-                    <div class="rectangle1">
-                        @if ($sampahKertas && file_exists(public_path('storage/fotos/' . $sampahKertas->foto)))
-                            <img src="{{ asset('storage/fotos/' . $sampahKertas->foto) }}" alt="{{ $sampahKertas->nmSampah }}" class="foto-sampah">
-                        @else
-                            <p>Foto tidak tersedia</p>
-                        @endif
-                    </div>
-                    <div class="rectanglea">
-                        <span class="tulisanjenis">{{ $sampahKertas->nmSampah ?? 'Nama Sampah tidak tersedia' }}</span>
-                    </div>
-                </div>
-                <div class="concontainer">
-                    <div class="rectangle1">
-                        @if ($sampahKardus && file_exists(public_path('storage/fotos/' . $sampahKardus->foto)))
-                            <img src="{{ asset('storage/fotos/' . $sampahKardus->foto) }}" alt="{{ $sampahKardus->nmSampah }}" class="foto-sampah">
-                        @else
-                            <p>Foto tidak tersedia</p>
-                        @endif
-                    </div>
-                    <div class="rectanglea">
-                        <span class="tulisanjenis">{{ $sampahKardus->nmSampah ?? 'Nama Sampah tidak tersedia' }}</span>
-                    </div>
-                </div>
-                <div class="concontainer">
-                    <div class="rectangle1">
-                        @if ($sampahPlastik && file_exists(public_path('storage/fotos/' . $sampahPlastik->foto)))
-                            <img src="{{ asset('storage/fotos/' . $sampahPlastik->foto) }}" alt="{{ $sampahPlastik->nmSampah }}" class="foto-sampah">
-                        @else
-                            <p>Foto tidak tersedia</p>
-                        @endif
-                    </div>
-                    <div class="rectanglea">
-                        <span class="tulisanjenis">{{ $sampahPlastik->nmSampah ?? 'Nama Sampah tidak tersedia' }}</span>
-                    </div>
+            <div class="containerawal">
+                <p>Kami disini untuk <span class="highlight">melayani</span>,<br> dan <span class="highlight">menyelamatkan</span> bumi!</p>
+                <div class="buttonmemilah">
+                    <button class="start-button">Mulai Memilah</button>
                 </div>
             </div>
-            <p class="keterangan">*Sampah yang dapat ditukar menjadi poin</p>
-        </div>
 
-        <div class="bjcon2">
-            <p class="tulewaste">E-Waste</p>
             
-        </div>
-    </div>
+            <div class="boxjualan">
+                
+                <div class="bjcon1">
+                        <div class="jenisdijual">
+                            <div class="concontainer">
+                                <div class="rectangle1">
+                                    @if ($sampahKertas && file_exists(public_path('storage/fotos/' . $sampahKertas->foto)))
+                                        <img src="{{ asset('storage/fotos/' . $sampahKertas->foto) }}" alt="{{ $sampahKertas->nmSampah }}" class="foto-sampah">
+                                    @else
+                                        <p>Foto tidak tersedia</p>
+                                    @endif
+                                </div>
+                                <div class="rectanglea">
+                                    <span class="tulisanjenis">{{ $sampahKertas->nmSampah ?? 'Nama Sampah tidak tersedia' }}</span>
+                                </div>
+                            </div>
+                            <div class="concontainer">
+                                <div class="rectangle1">
+                                    @if ($sampahKardus && file_exists(public_path('storage/fotos/' . $sampahKardus->foto)))
+                                        <img src="{{ asset('storage/fotos/' . $sampahKardus->foto) }}" alt="{{ $sampahKardus->nmSampah }}" class="foto-sampah">
+                                    @else
+                                        <p>Foto tidak tersedia</p>
+                                    @endif
+                                </div>
+                                <div class="rectanglea">
+                                    <span class="tulisanjenis">{{ $sampahKardus->nmSampah ?? 'Nama Sampah tidak tersedia' }}</span>
+                                </div>
+                            </div>
+                            <div class="concontainer">
+                                <div class="rectangle1">
+                                    @if ($sampahPlastik && file_exists(public_path('storage/fotos/' . $sampahPlastik->foto)))
+                                        <img src="{{ asset('storage/fotos/' . $sampahPlastik->foto) }}" alt="{{ $sampahPlastik->nmSampah }}" class="foto-sampah">
+                                    @else
+                                        <p>Foto tidak tersedia</p>
+                                    @endif
+                                </div>
+                                <div class="rectanglea">
+                                    <span class="tulisanjenis">{{ $sampahPlastik->nmSampah ?? 'Nama Sampah tidak tersedia' }}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="keterangan">*Sampah yang dapat ditukar menjadi poin</p>
+                </div>
+
+                <div class="bjcon2">
+                        <p class="tulewaste">E-Waste</p>
+                    
+                </div>
+            </div>
+         
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,14 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container-fluid">
+
         <div class="navbar-header">
             <a class="navbar-brand" href="{{ route('landingpage') }}">
                 <img src="{{ asset('images/WM LOGO WHITE.png') }}" alt="Logo" class="logo">
             </a>
-            <button class="signup-button d-lg-none">Sign Up</button>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
+        
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
@@ -18,18 +19,19 @@
                     <a class="nav-link {{ request()->routeIs('about') ? 'active' : '' }}" href="{{ route('about') }}">About</a>
                 </li>
             </ul>
-            <div class="search-container d-none d-lg-flex">
+            <div class="search-container">
                 <i class="fas fa-search search-icon"></i>
                 <input type="text" class="search-bar" placeholder="Cari kampus kamu disini!">
             </div>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a href="{{ route('logincust') }}" class="nav-link d-none d-lg-flex">Login</a>
+                    <a href="{{ route('logincust') }}" class="nav-link">Login</a>
                 </li>
                 <li class="nav-item">
                     <button class="signup-button">Sign Up</button>
                 </li>
             </ul>
         </div>
+        
     </div>
 </nav>
