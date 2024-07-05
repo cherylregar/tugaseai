54b5a567b80ad1febff02d06f69327a28d22357e
cherylregar <119674729+cherylregar@users.noreply.github.com>
2024-07-05 15:48:13 +0700

    Fix Landing Page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -9,6 +9,8 @@
 
 @section('content')
 
+<div id="top"></div> <!-- Tambahkan ini -->
+
 <div class="containerawal">
     <p>Kami disini untuk <span class="highlight">melayani</span>,<br> dan <span class="highlight">menyelamatkan</span> bumi!</p>
     <div class="buttonmemilah">
@@ -224,6 +226,36 @@
             </div>
         </div>
     </div>    
+    </div>
+
+ 
+    <div class="footer">
+        <div class="wmdansocmed">
+            <div class="logowmfooter">
+                <img src="{{ asset('images/WM LOGO WHITE.PNG') }}" alt="WM Logo">
+            </div>
+            <div class="lokasiwm">
+                Bandung, Indonesia
+            </div>
+            <div class="logosocmed">
+                <img src="{{ asset('images/linkedin.png') }}" class="linkedin" alt="LinkedIn">
+                <img src="{{ asset('images/instagram.png') }}" class="instagram" alt="Instagram">
+            </div>
+        </div>
+        <div class="footerdetail">
+            <div class="companycon">
+                <div class="tulisanjudul">Company</div>
+                <div class="tulisanfooter"><a href="#top">Home</a></div>
+                <div class="tulisanfooter"><a href="{{ url('/about') }}">About</a></div>
+                <div class="tulisanfooter"><a href="{{ url('/about') }}">Contact Us</a></div>
+            </div>
+            <div class="ourservicescon">
+                <div class="tulisanjudul">Our Services</div>
+                <div class="tulisanfooter">Sell Your Waste</div>
+                <div class="tulisanfooter">Wastepals</div>
+                <div class="tulisanfooter">Event Waste Manage</div>
+            </div>
+        </div>
     </div>
 
 
